e19d766c3368a9852be03523356fd69f82c905e4
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-05 23:30:13 +0700

    tugas_19_js
    tugas sudah ok !!!

---
+++ tugas-19a.js
@@ -0,0 +1,5 @@
+function ulangkata(){
+  var kata = "Semangat... ";
+  console.log(kata.repeat(5));
+}
+ulangkata();
---
+++ tugas-19b.js
@@ -0,0 +1,14 @@
+function ulangbintang(){
+
+  console.log("*".repeat(5));
+  console.log("*".repeat(20));
+  console.log("*".repeat(40));
+  console.log("*".repeat(60));
+  console.log("*".repeat(80));
+  console.log("*".repeat(60));
+  console.log("*".repeat(40));
+  console.log("*".repeat(20));
+  console.log("*".repeat(5));
+
+}
+ulangbintang();
